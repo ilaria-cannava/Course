a65bb334d682a1436330f8cf04accaa49ce76289
Ilaria-Cannava <ilaria.cnnv@gmail.com>
2024-03-25 15:57:48 +0000

    Final assignment

--- 11- Final assignment/churn.py
+++ 11- Final assignment/churn.py
@@ -23,32 +23,48 @@ def churn_prediction(input):
 
 # set up streamlit page
 def main():
-    st.set_page_config(page_title='Customer churn prediction', layout='wide')
+    #st.set_page_config(page_title='Customer churn prediction', layout='wide')
     
     # add image
     image = Image.open('customer_service_1.jpg')
     st.image(image, use_column_width=False)
+    add_selectbox =st.sidebar.selectbox('How would you liek to predict?',('Online','Batch'))
+    st.sidebar.info('This app will predict customer churn and identify customer churn risk')
+    st.sider.image(image)
     
     # add title
     st.title('Customer Churn Risk Prediction using Machine Learning')
-    st.write('Please enter relevnt customer data')
+    if add_selectbox =='Online':
+        
+        st.write('Please enter relevnt customer data')
     
-    # take input from user
-    df_selection =dff[['MonthlyMinutes', 'TotalRecurringCharge', 
-                       'PercChangeMinutes','UniqueSubs','Handsets', 
-                       'HandsetModels',
-                  'CurrentEquipmentDays', 'HandsetRefurbished',
-                  'HandsetWebCapable','RetentionCalls',
-                  'RetentionOffersAccepted', 'CreditRating']]
-    MonthlyMinutes = st.number_input('Monthly minutes usage:', min_value=0, step=1)
-    TotalRecurringCharge = st.number_input('Total recurring charges:', min_value=0, step=0.001)
-    PercChangeMinutes = st.number_input('Minutes usage change over the given period', min_value=None, max_value=None, step=0.001)
-    UniqueSubs = st.number_input('Number of Unique Subscritions:', )
-    Handsets = st.number_input
-    HandsetModels = st.number_input
-    CurrentEquipmentDays = st.number_input
-    HandsetRefurbished = st.number_input
-    HandsetWebCapable = st.number_input
-    RetentionCalls = st.number_input
-    RetentionOffersAccepted = st.number_input
-    CreditRating = st.number_input('Credit Rating, Range from 1 to 7 (Highest, High, Good, Medium,Good' '2-High' '5-Low, VeryLow ,Lowest')
+        # take input from user
+        MonthlyMinutes = st.number_input('Monthly minutes usage:', min_value=0, step=1)
+        TotalRecurringCharge = st.number_input('Total recurring charges:', min_value=0, step=0.001)
+        PercChangeMinutes = st.number_input('Minutes usage change over the given period', min_value=None, max_value=None, step=0.001)
+        UniqueSubs = st.number_input('Number of Unique Subscritions:', min_value=0, step=1)
+        Handsets = st.number_input('How many handsets the customer has:', min_value=0, step=1)
+        CurrentEquipmentDays = st.number_input('How many days is the current equipment old:' min_value=0, step=1)
+        HandsetRefurbished = st.number_input('Owns a refurbished handset| yes or no | yes = 1 and no = 0:',min_value=0, max_value=1, step=1)
+        HandsetWebCapable = st.number_input('Owns a web capable handset| yes or no | yes = 1 and no = 0:',min_value=0, max_value=1, step=1)
+        RetentionCalls = st.number_input('How many retention calls were made:', min_value=0, step=1)
+        RetentionOffersAccepted = st.number_input('Accepted retention offer| yes or no | yes = 1 and no = 0:',min_value=0, max_value=1, step=1)
+        CreditRating = st.number_input('Credit Rating, Range from 1 to 7 (Highest, High, Good, Medium, Good, Low, VeryLow ,Lowest)', min_value=1, max_value=7, step=1)
+    
+    # code for prediction
+    predict =''
+    
+    ## button for prediction
+    if st.button('Predict'):
+        predict = churn_prediction([MonthlyMinutes,TotalRecurringCharge,PercChangeMinutes,
+                                    UniqueSubs,Handsets,HandsetModels,CurrentEquipmentDays,
+                                    HandsetRefurbished, HandsetWebCapable, RetentionCalls,
+                                    RetentionOffersAccepted, CreditRating])
+    
+    st.success(predict)
+    
+    # run script
+main()
+
+# if __name__=='__main__':
+#     main()
