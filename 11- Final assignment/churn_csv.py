7f87b460532a0631a0f3f0c53acc3d5f580f276c
Ilaria-Cannava <ilaria.cnnv@gmail.com>
2024-03-26 09:15:26 +0000

    Final

--- 11- Final assignment/churn_csv.py
+++ 11- Final assignment/churn_csv.py
@@ -34,9 +34,18 @@ def main():
     st.write('Please enter relevant customer data or upload a CSV file.')
     
     # Option to input data manually
-    st.write('### Input Data Manually')
-    MonthlyMinutes = st.number_input('Monthly minutes usage:', min_value=-100000, step=1)
-    # Add other input fields here...
+    st.write('### Manually Input Data for a single customer or upload a csv file below')
+    MonthlyMinutes = st.number_input('Monthly minutes usage:', min_value=0, step=1)
+    TotalRecurringCharge = st.number_input('Total recurring charges:', min_value=0, step=1)
+    PercChangeMinutes = st.number_input('Minutes usage change over the given period', min_value=-1000, step=1)
+    UniqueSubs = st.number_input('Number of Unique Subscriptions:', min_value=0, step=1)
+    Handsets = st.number_input('How many handsets the customer has:', min_value=0, step=1)
+    CurrentEquipmentDays = st.number_input('How many days is the current equipment old:', min_value=0, step=1)
+    HandsetRefurbished = st.number_input('Owns a refurbished handset? (yes=1, no=0):', min_value=0, max_value=1, step=1)
+    HandsetWebCapable = st.number_input('Owns a web capable handset? (yes=1, no=0):', min_value=0, max_value=1, step=1)
+    RetentionCalls = st.number_input('How many retention calls were made:', min_value=0, step=1)
+    RetentionOffersAccepted = st.number_input('Accepted retention offer? (yes=1, no=0):', min_value=0, max_value=1, step=1)
+    CreditRating = st.number_input('Credit Rating (1 to 7):', min_value=1, max_value=7, step=1)
     
     # Option to upload CSV file
     st.write('### Upload CSV File')
@@ -61,7 +70,17 @@ def main():
     if st.button('Predict'):
         input_df = pd.DataFrame({
             'MonthlyMinutes': [MonthlyMinutes],
-            # Add other input fields here...
+            'Total recurring charges':[TotalRecurringCharge],
+            'Minutes usage change over the given period':[PercChangeMinutes],
+            'Number of Unique Subscriptions':[UniqueSubs],
+            'How many handsets the customer has':[Handsets],
+            'How many days is the current equipment old':[CurrentEquipmentDays],
+            'Owns a refurbished handset? (yes=1, no=0)':[HandsetRefurbished],
+            'Owns a web capable handset? (yes=1, no=0)':[HandsetWebCapable],
+            'How many retention calls were made':[RetentionCalls],
+            'Accepted retention offer? (yes=1, no=0)':[RetentionOffersAccepted],
+            'Credit Rating (1 to 7)':[CreditRating]
+            
         })
         prediction = churn_prediction(input_df)
         st.write(prediction)
